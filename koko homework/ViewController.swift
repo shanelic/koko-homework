02b19f49f940cb31625fe366926679672294b27e
shanelic_ <me@shanelic.io>
2023-11-12 15:49:06 +0800

    Initial Commit

---
+++ koko homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  koko homework
+//
+//  Created by Shane Li on 2023/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
