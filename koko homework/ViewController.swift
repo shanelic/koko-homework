7b2e3be1c42123f887369a10ea2fffb3871201e2
shanelic_ <me@shanelic.io>
2023-11-12 16:34:23 +0800

    feat(view): create entry buttons for the 3 situations

--- koko homework/ViewController.swift
+++ koko homework/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    private func setupUI() {
+        let stack = UIStackView()
+        view.addSubview(stack)
+        stack.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.spacing = 24
+        
+        let noFriendButton = UIButton()
+        noFriendButton.setTitle("  I have no friend.  ", for: .normal)
+        noFriendButton.setTitleColor(.black, for: .normal)
+        noFriendButton.layer.borderColor = UIColor.black.cgColor
+        noFriendButton.layer.borderWidth = 1.5
+        noFriendButton.layer.cornerRadius = 8
+        noFriendButton.addTarget(self, action: #selector(performNoFriend), for: .touchUpInside)
+        stack.addArrangedSubview(noFriendButton)
+        
+        let friendsButton = UIButton()
+        friendsButton.setTitle("  I have friends.  ", for: .normal)
+        friendsButton.setTitleColor(.black, for: .normal)
+        friendsButton.layer.borderColor = UIColor.black.cgColor
+        friendsButton.layer.borderWidth = 1.5
+        friendsButton.layer.cornerRadius = 8
+        friendsButton.addTarget(self, action: #selector(performFriends), for: .touchUpInside)
+        stack.addArrangedSubview(friendsButton)
+        
+        let friendsWithInvitationsButton = UIButton()
+        friendsWithInvitationsButton.setTitle("  I have friends and also invitations.  ", for: .normal)
+        friendsWithInvitationsButton.setTitleColor(.black, for: .normal)
+        friendsWithInvitationsButton.layer.borderColor = UIColor.black.cgColor
+        friendsWithInvitationsButton.layer.borderWidth = 1.5
+        friendsWithInvitationsButton.layer.cornerRadius = 8
+        friendsWithInvitationsButton.addTarget(self, action: #selector(performFriendsAndInvitations), for: .touchUpInside)
+        stack.addArrangedSubview(friendsWithInvitationsButton)
+    }
+    
+    @objc private func performNoFriend() {
+        print("::: I really have no any friend.")
+    }
+    
+    @objc private func performFriends() {
+        print("::: I have friends!")
+    }
+    
+    @objc private func performFriendsAndInvitations() {
+        print("::: I have friends and alse invitations!!")
+    }
 
 }
 
+#Preview {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    return storyboard.instantiateViewController(withIdentifier: "ViewController")
+}
