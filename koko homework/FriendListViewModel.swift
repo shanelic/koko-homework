3015575fe832d2b41eaca37ea340fa0b642c7545
shanelic_ <me@shanelic.io>
2023-11-16 03:48:20 +0800

    feat: view model and protocol delegation

--- koko homework/FriendListViewModel.swift
+++ koko homework/FriendListViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import Alamofire
+import PromiseKit
+
+class FriendListViewModel {
+    
+    weak public var delegate: FriendListDelegate?
+}
+
+protocol FriendListDelegate: AnyObject {
+    func fetchedData(user: User?)
+}
