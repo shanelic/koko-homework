c22b0d726fe700053930c838b8a58db44e64920c
shanelic_ <me@shanelic.io>
2023-11-16 03:42:53 +0800

    feat: new view files and assets

---
+++ koko homework/FriendListCell.swift
@@ -0,0 +1,8 @@
+//
+//  FriendCell.swift
+//  koko homework
+//
+//  Created by Shane Li on 2023/11/16.
+//
+
+import Foundation
---
+++ koko homework/TabbarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabbarController.swift
+//  koko homework
+//
+//  Created by Shane Li on 2023/11/15.
+//
+
+import Foundation
