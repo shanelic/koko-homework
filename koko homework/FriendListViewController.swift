12dbcf93ff36a4b327881f5ed03ab45dde37a73f
shanelic_ <me@shanelic.io>
2023-11-16 03:52:53 +0800

    feat: view - view model binding

--- koko homework/FriendListViewController.swift
+++ koko homework/FriendListViewController.swift
@@ -12,6 +12,8 @@ import SnapKit
 class FriendListViewController: UIViewController {
     
     let situation: Situation
+    let viewModel = FriendListViewModel()
+
 
     init(situation: Situation) {
         self.situation = situation
@@ -20,6 +22,21 @@ class FriendListViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.delegate = self
+        viewModel.initial(situation: situation)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
+
+extension FriendListViewController: FriendListDelegate {
+    func fetchedData(user: User?) {
     }
 }
 
