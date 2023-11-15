1bea9e5c89c87a64de348a868dbb13215b8af449
shanelic_ <me@shanelic.io>
2023-11-16 03:47:10 +0800

    feat: import UIColors

--- koko homework/Styles.swift
+++ koko homework/Styles.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+  @nonobjc class var appleGreen: UIColor {
+    return UIColor(red: 121.0 / 255.0, green: 196.0 / 255.0, blue: 27.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var brownGrey: UIColor {
+    return UIColor(white: 153.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var softPink: UIColor {
+    return UIColor(red: 249.0 / 255.0, green: 178.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var steel: UIColor {
+    return UIColor(red: 142.0 / 255.0, green: 142.0 / 255.0, blue: 147.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var whiteThree: UIColor {
+    return UIColor(white: 228.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var appleGreen40: UIColor {
+    return UIColor(red: 121.0 / 255.0, green: 196.0 / 255.0, blue: 27.0 / 255.0, alpha: 0.4)
+  }
+  @nonobjc class var greyishBrown: UIColor {
+    return UIColor(white: 71.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var steel12: UIColor {
+    return UIColor(red: 142.0 / 255.0, green: 142.0 / 255.0, blue: 147.0 / 255.0, alpha: 0.12)
+  }
+  @nonobjc class var pinkishGrey: UIColor {
+    return UIColor(white: 201.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var black10: UIColor {
+    return UIColor(white: 0.0, alpha: 0.1)
+  }
+  @nonobjc class var hotPink: UIColor {
+    return UIColor(red: 236.0 / 255.0, green: 0.0, blue: 140.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var booger: UIColor {
+    return UIColor(red: 166.0 / 255.0, green: 204.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var frogGreen: UIColor {
+    return UIColor(red: 86.0 / 255.0, green: 179.0 / 255.0, blue: 11.0 / 255.0, alpha: 1.0)
+  }
+}
