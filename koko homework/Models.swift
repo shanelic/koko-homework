2388bc697f7f6177d60efb0898872be6e2aadad8
shanelic_ <me@shanelic.io>
2023-11-16 03:47:35 +0800

    feat: data models

--- koko homework/Models.swift
+++ koko homework/Models.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    let name: String
+    let kokoid: String
+}
+
+struct Friend: Codable {
+    let name: String
+    let status: Status
+    var isFriend: Bool { status != .invitationReceived }
+    var isInvitation: Bool { status == .invitationReceived }
+    var isTop: Bool { _isTop == "1" }
+    private let _isTop: String
+    let friendID: String
+    private let _updateDate: String
+    var updateDate: Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        if let date = formatter.date(from: _updateDate) {
+            return date
+        }
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter.date(from: _updateDate)!
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case status
+        case _isTop = "isTop"
+        case friendID = "fid"
+        case _updateDate = "updateDate"
+    }
+    
+    enum Status: Int, Codable {
+        case invitationSent = 2
+        case invitationReceived = 0
+        case normal = 1
+    }
+}
+
+struct KokoResponse<T>: Codable where T: Codable {
+    let response: T
+}
